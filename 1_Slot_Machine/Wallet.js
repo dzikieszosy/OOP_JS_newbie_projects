61d838affd093f68b9902207daa6dabb45f13fff
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-06-30 13:33:55 +0200

    wallet.js started

---
+++ 1_Slot_Machine/Wallet.js
@@ -0,0 +1,10 @@
+class Wallet {
+   constructor(money) {
+      let _money = money;
+      //pobieranie aktualnej zawartości portfela
+      this.getWalletValue = () => _money;
+
+      //sprawdzanie czy można przystąpić do gry, czy ma tyle kasy
+      this.checkCanPlay = ()
+   }
+}
