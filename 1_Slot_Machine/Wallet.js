411e0393d3ef48e2d9a75c8f1074961ab9f5eb5b
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-07-03 08:59:45 +0200

    Wallet class finished

--- 1_Slot_Machine/Wallet.js
+++ 1_Slot_Machine/Wallet.js
@@ -5,6 +5,27 @@ class Wallet {
       this.getWalletValue = () => _money;
 
       //sprawdzanie czy można przystąpić do gry, czy ma tyle kasy
-      this.checkCanPlay = ()
+      this.checkCanPlay = (value) => {
+         if (_money >= value) return true;
+         return false;
+      }
+
+      //zmiana zawartości portfela
+      this.changeWallet = (value, type = "+") => {
+         if (typeof value === "number" && !isNaN(value)) { //!isNaN - czy jest NaN - odwrotnie
+            if (type === "+") {
+               return _money += value;
+            } else if (type === "-") {
+               return _money -= value;
+            } else {
+               throw new Error("nieprawidłowy typ działania");
+            }
+         } else {
+            console.log(typeof value);
+            throw new Error("nieprawidłowa liczba");
+         }
+      }
+
    }
 }
+const wallet = new Wallet(200);
